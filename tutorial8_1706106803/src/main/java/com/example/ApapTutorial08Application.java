c06816d26d0ac75b707339b88db3553cd2ebe029
lidianatalia <lidiamanik@outlook.com>
2018-04-27 16:55:06 +0700

    initial project and file writeup

---
+++ tutorial8_1706106803/src/main/java/com/example/ApapTutorial08Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapTutorial08Application
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (ApapTutorial08Application.class, args);
+    }
+}
